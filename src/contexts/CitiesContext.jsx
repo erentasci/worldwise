9595d00ac6e153b820d325354eaca0c0b235e105
Eren Tasci <erent4sci@gmail.com>
2023-12-10 21:29:47 +0300

    feat: finishing city view

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const BASE_URL = "http://localhost:8000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
@@ -24,11 +25,27 @@ function CitiesProvider({ children }) {
 
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert("There was an error fetching cities.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        getCity,
+        currentCity,
       }}>
       {children}
     </CitiesContext.Provider>
